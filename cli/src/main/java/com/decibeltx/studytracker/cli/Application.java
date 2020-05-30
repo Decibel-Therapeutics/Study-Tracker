4ac8820830f13cdc0f98f4dcb8655e151a951931
woemler <woemler@decibeltx.com>
2020-05-30 12:23:12 -0400

    Added some documentation for the CLI. Fixed issue with the web application packaging.

--- cli/src/main/java/com/decibeltx/studytracker/cli/Application.java
+++ cli/src/main/java/com/decibeltx/studytracker/cli/Application.java
@@ -3,9 +3,11 @@ package com.decibeltx.studytracker.cli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @Configuration
+@PropertySource("defaults.properties")
 public class Application {
 
   public static void main(String[] args) {
