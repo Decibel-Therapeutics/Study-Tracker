5b9cf7a69d96c86019f9ebabb851c2db35ffaa3e
woemler <woemler@decibeltx.com>
2021-11-02 21:25:36 -0400

    Improved results page styling. Added favicon.

--- react/components/search.js
+++ react/components/search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Field, Form, Formik} from 'formik';
 import {
   Card,
@@ -81,52 +81,99 @@ export const SearchHits = ({hits}) => {
 
 }
 
+const SearchHitHighlight = ({field, text}) => {
+  return (
+      <Col lg={12} className={"search-hit-highlight"}>
+        <h6>{field}</h6>
+        <blockquote>
+          <div className="bg-light p-2 font-italic" dangerouslySetInnerHTML={createMarkup(text)}/>
+        </blockquote>
+      </Col>
+  )
+}
+
+const SearchHitHighlights = ({hit}) => {
+  let list = [];
+  for (const [field, highlight] of Object.entries(hit.highlightFields)) {
+    // const bits = field.split(".");
+    // const field = bits[bits.length-1];
+    highlight.forEach(h => {
+      const text = h.replace("<em>", "<mark>").replace("</em>", "</mark>");
+      list.push(
+          <SearchHitHighlight
+            key={"search-highlight-" + hit.document.id + "-" + field}
+            field={field}
+            text={text}
+          />
+      );
+    });
+  }
+  return (
+    <Col sm={12}>
+      <h6>Matched Fields</h6>
+      <Row>
+        {list}
+      </Row>
+    </Col>
+  );
+}
+
 const createMarkup = (content) => {
   return {__html: content};
 };
 
 const SearchHit = ({hit}) => {
+  const [toggle, setToggle] = useState(false);
   const study = hit.document;
-  let highlights = '';
-  if (!!hit.highlightFields) {
-    let f = [];
-    for (const [fields, highlight] of Object.entries(hit.highlightFields)) {
-      const bits = fields.split(".");
-      const field = bits[bits.length-1];
-      highlight.forEach(h => {
-        const text = h.replace("<em>", "<mark>").replace("</em>", "</mark>");
-        f.push(
-            <Col key={"search-highlight-" + hit.document.id + "-" + fields} lg={12} className={"search-hit-highlight"}>
-              <h6>{fields}</h6>
-              <blockquote>
-                <div className="bg-light p-2 font-italic" dangerouslySetInnerHTML={createMarkup(text)}/>
-              </blockquote>
-            </Col>
-        );
-      })
-    }
-    highlights = f;
-  }
   return (
       <Card>
         <CardBody>
           <Row>
 
-            <Col lg={12}>
+            <Col sm={8} md={10}>
               <h4>
                 <a href={"/study/" + study.code}>{study.code}: {study.name}</a>
               </h4>
-              {/*<h5>{study.program.name}</h5>*/}
+            </Col>
+
+            <Col sm={4} md={2}>
+              <p className="text-muted">
+                <span className="float-right">{study.program.name}</span>
+              </p>
+            </Col>
+
+            <Col sm={12}>
               <p>{study.description}</p>
             </Col>
 
-            {highlights}
-
-            <Col lg={12}>
-              <p className="text-muted text-sm">
-                <span className="float-right">Score: {hit.score}</span>
-              </p>
+            <Col xs={12}>
+              <a href={"/study/" + study.code} className="btn btn-sm btn-outline-primary">
+                View Study
+              </a>
+              &nbsp;&nbsp;
+              <a className="btn btn-sm btn-outline-secondary" onClick={() => setToggle(!toggle)}>
+                {!!toggle ? "Hide": "Show"} Hit Details
+              </a>
             </Col>
+
+            <div hidden={!toggle} style={{width: "100%"}}>
+
+              <Col xs={12}>
+                <hr />
+              </Col>
+
+              <Col xs={12}>
+                <h6>Search Score</h6>
+                <p>{hit.score}</p>
+              </Col>
+
+              {
+                !!hit.highlightFields
+                    ? <SearchHitHighlights hit={hit} />
+                    : ''
+              }
+
+            </div>
 
           </Row>
         </CardBody>
